90b5725801be939239bfdb32d04eb67fb47e40af
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-03 17:21:51 +0530

    add

--- src/app/modules/security/security-routing.module.ts
+++ src/app/modules/security/security-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SecurityContextComponent} from "./component/security-context/security-context.component";
+import {LoginComponent} from "./component/login/login.component";
+import {SignupComponent} from "./component/signup/signup.component";
+import {ForgotPasswordComponent} from "./component/forgot-password/forgot-password.component";
+import {OtpVerificationComponent} from "./component/otp-verification/otp-verification.component";
+import {ResetPasswordComponent} from "./component/reset-password/reset-password.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/security/context',pathMatch:'full'},
+  {path:'context',component:SecurityContextComponent,children:[
+      {path:'',redirectTo:'/security/context/login',pathMatch:'full'},
+      {path:'login',component:LoginComponent},
+      {path:'signup',component:SignupComponent},
+      {path:'forgot-password',component:ForgotPasswordComponent},
+      {path:'confirm-otp',component:OtpVerificationComponent},
+      {path:'reset-password',component:ResetPasswordComponent},
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
