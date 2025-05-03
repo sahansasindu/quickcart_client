4f2516c439979a3af0f73ab7e3327c96b6694170
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-03 17:20:27 +0530

    add

--- src/app/modules/process/process-routing.module.ts
+++ src/app/modules/process/process-routing.module.ts
@@ -1,29 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {SecurityContextComponent} from "../security/component/security-context/security-context.component";
-import {LoginComponent} from "../security/component/login/login.component";
-import {SignupComponent} from "../security/component/signup/signup.component";
-import {ForgotPasswordComponent} from "../security/component/forgot-password/forgot-password.component";
-import {OtpVerificationComponent} from "../security/component/otp-verification/otp-verification.component";
-import {ResetPasswordComponent} from "../security/component/reset-password/reset-password.component";
+import {ContextComponent} from "./component/context/context.component";
+import {HomepageComponent} from "./component/homepage/homepage.component";
 
 const routes: Routes = [
 
 
-  {path:'',redirectTo:'/security/context',pathMatch:'full'},
-  {path:'context',component:SecurityContextComponent,children:[
-      {path:'',redirectTo:'/security/context/login',pathMatch:'full'},
-      {path:'login',component:LoginComponent},
-      {path:'signup',component:SignupComponent},
-      {path:'forgot-password',component:ForgotPasswordComponent},
-      {path:'confirm-otp',component:OtpVerificationComponent},
-      {path:'reset-password',component:ResetPasswordComponent},
-
-    ]
-  }
-
-
-
+  {path:'',redirectTo:'/process/context',pathMatch:'full'},
+  {path:'context',component:ContextComponent,children:[
+      {path:'',redirectTo:'/process/context/home',pathMatch:'full'},
+      {path:'home',component:HomepageComponent}
+    ]}
 
 ];
 
