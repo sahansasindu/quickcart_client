7c4f182425b03d7f637df547d4e4b47966f5f157
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-03 17:13:03 +0530

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import {NotfoundpageComponent} from "./components/notfoundpage/notfoundpage.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {path:'',redirectTo:'/process',pathMatch:'full'},
+  {
+    path:'process',loadChildren:
+      ()=>import('./modules/process/process.module').then(e=>e.ProcessModule)
+  },
+  {
+    path:'security',loadChildren:
+      ()=>import('./modules/security/security.module').then(e=>e.SecurityModule)
+  },
+  {
+    path:'dashboard',loadChildren:
+      ()=>import('./modules/dashboard/dashboard.module').then(e=>e.DashboardModule)
+  },
+
+  {path:'**',component:NotfoundpageComponent}
+
+
+
+];
