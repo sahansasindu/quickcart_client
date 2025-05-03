e1216940f7db8dd436f59afff132bc79567d555c
sahansasindu <sahan_2020742@fot.ruh.ac.lk>
2025-05-03 17:10:12 +0530

    add

---
+++ src/app/modules/security/component/security-header/security-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-security-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './security-header.component.html',
+  styleUrl: './security-header.component.scss'
+})
+export class SecurityHeaderComponent {
+
+}
